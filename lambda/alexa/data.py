aa55a0081c5ff6b645d49a522f64e4e20283ad9f
Aastha Agrawal <aasthaagrawal94@gmail.com>
2020-04-05 13:35:01 -0400

    Added Validation intent

--- lambda/alexa/data.py
+++ lambda/alexa/data.py
@@ -4,7 +4,9 @@ WELCOME_MSG = _("Welcome to Speech Therapy Practice. What do you want to practic
 WELCOME_REMPROMPT_MSG = _("I want to practice sentences with r sound. What do you want to practice today?")
 CREATE_PRACTICE_MSG =  _("Topic selected is {}. Am I correct?")
 TOPIC_MISSING_MSG = _("What do you want to practice today?")
-START_PRACTICE_MSG = _("Please repeat after me: {}")
+START_PRACTICE_MSG = _("Please say the word 'Speech' and then repeat after me: {}")
+VALIDATION_INCORRECT_USER_SENTENCE = _("Your said {}. Let's try again! Please say the word 'Speech' and then repeat after me: {}")
+VALIDATION_CORRECT_USER_SENTENCE = _("Do you want to quit or continue?")
 WELCOME_BACK_MSG = _(
     "Welcome back. It looks like there is {} day until your {}th birthday.")
 WELCOME_BACK_MSG_plural = _(
